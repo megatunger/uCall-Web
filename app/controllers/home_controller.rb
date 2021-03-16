471e2301e874b1bf3ec046150f67ba35b6e71a89
Hoang Son Tung <megatunger@gmail.com>
2021-03-16 17:04:04 +0700

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
+require 'net/https'
+
 class HomeController < ActionController::Base
   layout 'application'
+  before_action :set_variables
   def index
-    @app_link = "https://ucall-app.etronresearch.work/"
+  end
+
+  def redirect_demo
+    uri = URI(@api_link)
+    http = ::Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = ::Net::HTTP::Post.new(uri.path + params.require(:call_request).permit(:name, :phone, :gender).to_query(:demo), {'Content-Type' => 'application/json'},)
+    response = http.request(request)
+    body = JSON.parse(response.body)
+    render plain: @app_link + 'demo/' + body['userId']
+  end
+
+  private
+  def set_variables
+    @app_link = 'https://ucall-app.etronresearch.work/'
+    @api_link = 'https://ucall-api.etronresearch.work/'
   end
 end

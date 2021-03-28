3e811f994e95660309e645e064d2ccc50f8ef1d6
Hoang Son Tung <megatunger@gmail.com>
2021-03-28 09:27:27 +0700

    update call api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,16 +9,17 @@ class HomeController < ActionController::Base
   def redirect_demo
     uri = URI(@api_link)
     http = ::Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = ::Net::HTTP::Post.new(uri.path + params.require(:call_request).permit(:name, :phone, :gender).to_query(:demo), {'Content-Type' => 'application/json'},)
+    # http.use_ssl = true
+    query_string = params.require(:call_request).permit(:name, :pronoun, :phone_number).to_query('')
+    request = ::Net::HTTP::Post.new(uri.path + query_string, {'Content-Type' => 'application/json', "Authorization" => "Bearer lSHQbeevaDaJQclsd7Ul20U5Cto1"},)
     response = http.request(request)
-    body = JSON.parse(response.body)
-    render plain: @app_link + 'demo/' + body['userId']
+    # render plain: @app_link + 'demo/' + body['userId']
+    redirect_to root_path
   end
 
   private
   def set_variables
     @app_link = 'https://ucall-app.etronresearch.work/'
-    @api_link = 'https://ucall-api.etronresearch.work/'
+    @api_link = 'http://api.etronresearch.work:10101/v1/api/make-call'
   end
 end
